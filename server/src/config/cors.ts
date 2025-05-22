d5aaf63a1f161f888a3ad1a8de7e9c446985b8b1
Angel Contreras <c3po.angel@gmail.com>
2025-05-22 12:06:29 -0600

    cors fix

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -2,11 +2,13 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [process.env.FRONTEND_URL];
+    if (process.argv.includes("--api")) {
+      whitelist.push(undefined);
+    }
     if (whitelist.includes(origin)) {
       callback(null, true);
-    }
-    else{
-        callback(new Error("Error de CORS"))
+    } else {
+      callback(new Error("Error de CORS"));
     }
   },
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/bd";
+import authRoutes from "./routes/authRoutes";
 import projectRouter from "./routes/projectRoutes";
 import cors from "cors";
 import { corsConfig } from "./config/cors";
@@ -9,8 +10,9 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(cors(corsConfig));
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
 //Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRouter);
 export default app;
